defd7a3b58dda5f773db0babb81cab55a3e44676
Lukas Degener <l.degener@tarent.de>
2021-04-12 23:51:20 +0200

    allow adding/removing locations

--- src/App.svelte
+++ src/App.svelte
@@ -2,9 +2,13 @@
 	//import 'bulma/css/bulma.css';
 	import Papa from "papaparse";
     import Location from "./Location.svelte";
-	let showLocationModal = false;
+    import Select from 'svelte-select';
+    import { locations } from './stores.js';
+        let showLocationModal = false;
+        let showSelect = false;
 	let location_name;
-	
+	/*let locations = ["United Kingdom", "Germany",  "World"]*/
+
 	let showLocation = function(name) {
 		location_name = name;
 		showLocationModal = true;
@@ -12,6 +16,7 @@
 
 	let url = "https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/vaccinations.csv"
 	let headlines = [];
+        let records = [];
 	let trends = {};
 
 	let location_by_name = function(data, name) {
@@ -30,9 +35,9 @@
 		return(Object.values(selected));
 	}
 
-	let select_countries = function(elem) {
-		let interesting_countries = ["United Kingdom", "Germany", "World"]
-		return interesting_countries.includes(elem["location"])
+        let negate = (f)=>(...args)=>!f(...args);
+        let select_countries = (locations)=>(elem)=> {
+		return locations.includes(elem["location"])
 	}
 
 	// maps values linearly between the floor and ceiling
@@ -43,37 +48,67 @@
 	}
 
 	Papa.parse(url, {download:true, header: true, complete: function(results) {
-		let parsed = results.data.map(row => {
+		records = results.data.map(row => {
 			row.date = new Date(row.date);
 			return(row)
 		}).filter(row => {
 			return(row.location !== '')
 		});
 
-		let filtered = parsed.filter(select_countries)
 
-		headlines = one_from_group(
-			filtered,
-			element => { return(element.location) },
-			(a,b) => { return(a.date > b.date ? a : b) }
-		);
-
-		filtered.forEach(({location, daily_vaccinations}) => {
-			let value = Number(daily_vaccinations);
-			trends[location] = location in trends ? trends[location].concat(value) : [value];
-		})
-
-		for(var location in trends) {
-			trends[location] = rescale(trends[location],0,100);
-			if(isNaN(trends[location][0])) {
-				trends[location] = [];
-			}
-		}
 	}});
+  const uniq = (sel)=>(accu,elm)=>{
+    const prev = accu[accu.length - 1];
+    if(prev && sel(prev) === sel(elm)){
+      return accu;
+    } else {
+      return [...accu, elm];
+    }
+  };
+  $: filtered = records.filter(select_countries($locations));
+  $: options = records
+    .filter(negate(select_countries($locations)))
+    .map(({location})=>({label:location,value:location}))
+    .reduce(uniq(x=>x.value),[]);
+  $: headlines = one_from_group(
+            filtered,
+            element => { return(element.location) },
+            (a,b) => { return(a.date > b.date ? a : b) }
+    );
+  $: {
+    filtered.forEach(({location, daily_vaccinations}) => {
+            let value = Number(daily_vaccinations);
+            trends[location] = location in trends ? trends[location].concat(value) : [value];
+    })
+
+    for(var location in trends) {
+            trends[location] = rescale(trends[location],0,100);
+            if(isNaN(trends[location][0])) {
+                    trends[location] = [];
+            }
+    }
+  };
+  const handle_remove = (location)=>(ev)=>{
+    $locations = $locations.filter(x=>x!==location);
+  };
+  const handle_add = (ev)=>{
+    showSelect = true;
+  };
+  const handle_select = (ev)=>{
+    $locations = [...$locations, ev.detail.value];
+    showSelect = false;
+  };
 </script>
 
 <section>
-	{#each headlines as { location, total_vaccinations, total_vaccinations_per_hundred, state }, i}
+        <div class="menu">
+        {#if showSelect}
+          <Select class="select-add" items={options} on:select={handle_select} bind:isFocused={showSelect}></Select>
+        {:else}
+          <button class="button-add" on:click|preventDefault|stopPropagation={handle_add}>Add</button>
+        {/if}
+        </div>
+	{#each headlines as { location, total_vaccinations, total_vaccinations_per_hundred, state } (location)}
 	<div class="thing" on:click="{showLocation(location)}">
 		<svg>
 			<g>
@@ -86,8 +121,9 @@
 			{location}
 		</h1>
 		<h2>
-			{Number(total_vaccinations).toLocaleString()}	
+			{Number(total_vaccinations).toLocaleString()}
 		</h2>
+                <button class="button-remove" on:click|preventDefault|stopPropagation={handle_remove(location)}>Remove</button>
 	</div>
 	{/each}
 </section>
@@ -96,6 +132,43 @@
 	section {
 		position: relative;
 	}
+        .menu {
+		background-color: rgb(118, 173, 194);
+		width: 16em;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 1em;
+		padding-top: .25em;
+		padding-bottom: .25em;
+		padding-left: 1em;
+		padding-right: 1em;
+		border-radius: 1em;
+		display: block;
+		position: relative;
+        }
+        .thing > .button-remove {
+          border:none;
+          color:white;
+          background:rgba(0,0,0,0);
+          position: absolute;
+          display: block;
+          top: 1em;
+          right: 1em;
+          margin: 0;
+          padding: 0;
+        }
+        .button-add {
+                margin-left: auto;
+                margin-right: auto;
+                display: block;
+                width: 100%;
+                background: rgba(0,0,0,0);
+                border:none;
+                margin-bottom: 0px;
+                margin-top: 0px;
+                color: white;
+                padding:0;
+        }
 	.thing {
 		background-color: rgb(118, 173, 194);
 		color: white;
